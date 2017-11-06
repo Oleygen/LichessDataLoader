ae65b57b8e6ff50b3cdaeb257726d65ba15ebdb4
Ilya <inkeed@gmail.com>
2017-11-07 00:17:01 +0200

    Initial Commit

---
+++ LichessDataLoader/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  LichessDataLoader
+//
+//  Created by Gennadii on 11/7/17.
+//  Copyright © 2017 Gennady Oleynik. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
